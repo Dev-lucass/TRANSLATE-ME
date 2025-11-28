d3f73c29752b20aed0f5aea319d3a76c3bc98fdf
Dev-lucass <lucasbitencourt959@gmail.com>
2025-11-28 17:39:21 -0300

    Initial commit

---
+++ src/main/java/com/example/TranslateMe/API/TranslateMeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TranslateMe.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateMeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateMeApiApplication.class, args);
+	}
+
+}
